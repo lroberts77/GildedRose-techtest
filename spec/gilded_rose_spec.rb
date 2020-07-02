5a055ad95c7e184730727b8670f066dd96e1f08d
liam roberts <liamroberts77@googlemail.com>
2020-07-02 15:02:48 +0100

    added test for sell in date decreasing by 1 each day

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,12 +2,22 @@ require 'gilded_rose'
 
 describe GildedRose do
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
-    end
-  end
+    describe "#update_quality" do
 
+        context "Generic item" do
+
+            it "does not change the name" do
+                items = [Item.new("foo", 0, 0)]
+                GildedRose.new(items).update_quality()
+                expect(items[0].name).to eq "foo"
+            end
+
+            it "All items have a sell_in date which decreases by 1 each day" do
+                items = [Item.new("item", 6, 25)]
+                GildedRose.new(items).update_quality()
+                GildedRose.new(items).update_quality()
+                expect(items[0].sell_in).to eq (4)
+            end
+        end    
+    end
 end
