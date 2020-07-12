c5c4c6fccab0b9573b8abb99ece63b73b5e32448
liam roberts <liamroberts77@googlemail.com>
2020-07-12 22:06:27 +0100

    added test for item quality after sell in date has passed

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -31,6 +31,12 @@ describe GildedRose do
                 GildedRose.new(items).update_quality()
                 expect(items[0].quality).to eq (0)
             end
+
+            it "item has quality value which decreases by 2 each day after sell_in date" do
+                items = [Item.new("item", 0, 10)]
+                GildedRose.new(items).update_quality()
+                expect(items[0].quality).to eq (8)
+              end
         end
     end
 end
