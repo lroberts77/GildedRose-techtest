2ab0ab52194c92df2dd828ab44a0dd7f6bbb0d4b
liam roberts <liamroberts77@googlemail.com>
2020-07-13 16:02:04 +0100

    added test for backstage pass quality when sell in less than 5 days

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -74,6 +74,12 @@ describe GildedRose do
                 GildedRose.new(items).update_quality()
                 expect(items[0].quality).to eq (12)
             end
+
+            it "increases in quality by 3 when sell_in 5 days or less" do
+                items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+                GildedRose.new(items).update_quality()
+                expect(items[0].quality).to eq (13)
+            end
         end
     end
 end
